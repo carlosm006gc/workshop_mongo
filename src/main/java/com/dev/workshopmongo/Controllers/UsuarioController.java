164ea1699584366a9e81981ce8a551c9230a31d5
carlosm006gc <104165201+carlosm006gc@users.noreply.github.com>
2022-10-20 20:35:45 -0300

    Metodo Post de usuarios funcionando

--- src/main/java/com/dev/workshopmongo/Controllers/UsuarioController.java
+++ src/main/java/com/dev/workshopmongo/Controllers/UsuarioController.java
@@ -1,13 +1,17 @@
 package com.dev.workshopmongo.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dev.workshopmongo.dto.UsuarioDto;
 import com.dev.workshopmongo.entities.Usuario;
@@ -16,15 +20,31 @@ import com.dev.workshopmongo.services.UsuarioService;
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioController {
-	
+
 	@Autowired
 	private UsuarioService service;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<UsuarioDto>> findAll(){
+	public ResponseEntity<List<UsuarioDto>> findAll() {
 		List<Usuario> list = service.findAll();
 		List<UsuarioDto> listDto = list.stream().map(x -> new UsuarioDto(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
-	} 
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UsuarioDto> findById(@PathVariable String id) {
+		Usuario usuario = service.findById(id);
+		return ResponseEntity.ok().body(new UsuarioDto(usuario));
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UsuarioDto usuarioDto) {
+		Usuario usuario = service.fromDto(usuarioDto);
+		usuario = service.insert(usuario);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(usuario.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
 
 }
--- src/main/java/com/dev/workshopmongo/services/UsuarioService.java
+++ src/main/java/com/dev/workshopmongo/services/UsuarioService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dev.workshopmongo.dto.UsuarioDto;
 import com.dev.workshopmongo.entities.Usuario;
 import com.dev.workshopmongo.repository.UsuarioRepository;
 import com.dev.workshopmongo.services.exception.ObjectNotFoundException;
@@ -20,8 +21,16 @@ public class UsuarioService {
 		return repository.findAll();
 	}
 	
-	public Usuario findAllId(String id) {
+	public Usuario findById(String id) {
 		Optional<Usuario> usuario = repository.findById(id);
 		return usuario.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public Usuario insert(Usuario usuario) {
+		return repository.insert(usuario);
+	}
+	
+	public Usuario fromDto(UsuarioDto usuarioDto) {
+		return new Usuario(usuarioDto.getId(),usuarioDto.getNome(), usuarioDto.getEmail());
 	}
 }
